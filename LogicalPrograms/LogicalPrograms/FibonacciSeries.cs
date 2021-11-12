9ab2344a947bae9b43c2cbe0c14416645e32f85a
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-12 12:32:36 +0530

    [Gyanendra] Add: A Program to Check Fibonacci Series

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class FibonacciSeries
+    {
+        public static int fibonacciSeries(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
+        }
+        public void series(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", fibonacciSeries(i));
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Logical Program Press- 1-Fibonacci Series, 2-Perfect Number, 3-Prime Number, 4-Reverse Number, 5-Coupan Number, 6-Stopwatch Program, 7-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the length of the Fibonacci Series: ");
+                        int length = Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries fc = new FibonacciSeries();
+                        fc.series(length);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
